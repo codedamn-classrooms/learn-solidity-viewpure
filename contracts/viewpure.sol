b4006a69fa6e0f4c2ae0948808835a9a7951a90d
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-21 18:48:19 +0530

    Update viewpure.sol

--- contracts/viewpure.sol
+++ contracts/viewpure.sol
@@ -7,20 +7,20 @@ contract viewpure {
     uint256 b = 2;
     uint256 e = 5;
 
-    function variableA() public view returns (uint256) {
+    function variableA() public returns (uint256) {
         return a;
     }
 
-    function variableB() public view returns (uint256) {
+    function variableB() public returns (uint256) {
         return b + 1;
     }
 
-    function variableC() public pure returns (uint256) {
+    function variableC() public returns (uint256) {
         uint256 c = 3;
         return c;
     }
 
-    function variableD() public pure returns (uint256) {
+    function variableD() public returns (uint256) {
         uint256 d = 4;
         return d + 1;
     }
